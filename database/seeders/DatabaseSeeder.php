9cb5d3a4249f9a149dec672069de49d9c3e1a978
Angad Yadav <angadyadavv91@gmail.com>
2022-02-21 18:06:10 +0530

    Added status seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CustomerSeeder::class
             ]);
+        $this->call([
+            Status::class
+            ]);
     }
 }
